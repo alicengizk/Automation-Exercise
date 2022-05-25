50c611da04d506b721035ac060c0180a591ba98f
alicengizk <alicengizk@gmail.com>
2022-05-25 12:15:28 +0300

    All Test Done

--- src/test/java/Automation_Exercise/Test_Case25_Verify_Scroll_Up_using_Arrow_button_and_Scroll_Down_functionality.java
+++ src/test/java/Automation_Exercise/Test_Case25_Verify_Scroll_Up_using_Arrow_button_and_Scroll_Down_functionality.java
@@ -1,5 +1,11 @@
 package Automation_Exercise;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import utilities.TestBase;
 
 public class Test_Case25_Verify_Scroll_Up_using_Arrow_button_and_Scroll_Down_functionality extends TestBase {
@@ -13,4 +19,18 @@ public class Test_Case25_Verify_Scroll_Up_using_Arrow_button_and_Scroll_Down_fun
     6. Click on arrow at bottom right side to move upward
     7. Verify that page is scrolled up and 'Full-Fledged practice website for Automation Engineers' text is visible on screen
      */
+
+    @Test
+    public void test25() {
+        Actions actions=new Actions(driver);
+        driver.get("http://automationexercise.com");
+        WebElement homepage=driver.findElement(By.xpath("//body"));
+        Assert.assertTrue(homepage.isDisplayed());
+        actions.sendKeys(Keys.CONTROL,Keys.END).perform();
+        driver.findElement(By.xpath("//*[text()='Subscription']")).isDisplayed();
+        driver.findElement(By.id("scrollUp")).click();
+        driver.findElement(By.xpath("//*[text()='Full-Fledged practice website for Automation Engineers']")).isDisplayed();
+
+
+    }
 }
--- src/test/java/Automation_Exercise/Test_Case26_Verify_Scroll_Up_without_Arrow_button_and_Scroll_Down_functionality.java
+++ src/test/java/Automation_Exercise/Test_Case26_Verify_Scroll_Up_without_Arrow_button_and_Scroll_Down_functionality.java
@@ -1,5 +1,11 @@
 package Automation_Exercise;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import utilities.TestBase;
 
 public class Test_Case26_Verify_Scroll_Up_without_Arrow_button_and_Scroll_Down_functionality extends TestBase {
@@ -13,4 +19,16 @@ public class Test_Case26_Verify_Scroll_Up_without_Arrow_button_and_Scroll_Down_f
     6. Scroll up page to top
     7. Verify that page is scrolled up and 'Full-Fledged practice website for Automation Engineers' text is visible on screen
      */
+
+    @Test
+    public void test26() {
+        Actions actions=new Actions(driver);
+        driver.get("http://automationexercise.com");
+        WebElement homeage=driver.findElement(By.xpath("//body"));
+        Assert.assertTrue(homeage.isDisplayed());
+        actions.sendKeys(Keys.CONTROL,Keys.END).perform();
+        actions.sendKeys(Keys.CONTROL,Keys.UP).perform();
+        driver.findElement(By.xpath("//*[text()='Full-Fledged practice website for Automation Engineers']")).isDisplayed();
+
+    }
 }
